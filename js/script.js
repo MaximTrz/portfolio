1e374dc722c55ab0f871f5fc8bc3863973e439a0
Maxim <maxim.trz@gmail.com>
2019-04-11 17:01:04 +0500

    11/04/2019

--- js/script.js
+++ js/script.js
@@ -5,6 +5,53 @@ window.addEventListener('DOMContentLoaded', ()=>{
     buttonMenuBox.addEventListener("click", ()=>{
         menuList.classList.toggle("hideSM");
         menuList.classList.toggle("showSM");
-    });    
+    });
     
 });
+
+
+const divider = document.querySelector(".divider"),
+        leftBlock = document.querySelector(".imageBlock_left"),
+        rightBlock = document.querySelector(".imageBlock_right"),
+        wrapper = document.querySelector(".imageBox"),
+        imgLeft = document.querySelector(".imageBlock_left>img");
+        imgRight = document.querySelector(".imageBlock_right>img");
+        
+    leftBlock.style.width = wrapper.offsetWidth + divider.offsetWidth +'px';
+    leftBlock.style.height = wrapper.offsetHeight + 'px';
+    imgLeft.style.width = wrapper.offsetWidth + divider.offsetWidth +'px';
+
+    rightBlock.style.width = wrapper.offsetWidth + divider.offsetWidth + 'px';
+    rightBlock.style.height = wrapper.offsetHeight + 'px';
+    imgRight.style.width = wrapper.offsetWidth + divider.offsetWidth + 'px';
+
+    divider.style.left = "50%";
+    leftBlock.style.width = (wrapper.offsetWidth/2) + 'px';
+    
+
+  divider.addEventListener('mousedown', (e)=>{
+
+    function moveAt(e) {
+      let posX = e.pageX-divider.offsetWidth / 2;
+  
+      if ((posX>=0)&&(posX<=wrapper.offsetWidth*2)){
+        divider.style.left = posX  + 'px';
+        leftBlock.style.width = posX + 'px';        
+      }      
+    }
+
+    wrapper.onmousemove = function(e) {
+      moveAt(e);
+      console.log(e.pageX);
+      console.log(divider.getBoundingClientRect());
+     }
+});
+
+  divider.addEventListener("mouseup", ()=>{
+    wrapper.onmousemove = null;
+    divider.onmouseup = null;
+  });
+
+  wrapper.onmousedown = function(){
+      return false;
+  };
