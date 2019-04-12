a9e00569e5f1e563dcb91de904982ec424be4a5a
Maxim <maxim.trz@gmail.com>
2019-04-12 11:34:31 +0500

    12/04/2019

--- js/script.js
+++ js/script.js
@@ -6,17 +6,15 @@ window.addEventListener('DOMContentLoaded', ()=>{
         menuList.classList.toggle("hideSM");
         menuList.classList.toggle("showSM");
     });
-    
-});
 
 
-const divider = document.querySelector(".divider"),
-        leftBlock = document.querySelector(".imageBlock_left"),
-        rightBlock = document.querySelector(".imageBlock_right"),
-        wrapper = document.querySelector(".imageBox"),
-        imgLeft = document.querySelector(".imageBlock_left>img");
-        imgRight = document.querySelector(".imageBlock_right>img");
-        
+    const divider = document.querySelector(".divider"),
+          leftBlock = document.querySelector(".imageBlock_left"),
+          rightBlock = document.querySelector(".imageBlock_right"),
+          wrapper = document.querySelector(".imageBox"),
+          imgLeft = document.querySelector(".imageBlock_left>img");
+          imgRight = document.querySelector(".imageBlock_right>img");
+          
     leftBlock.style.width = wrapper.offsetWidth + divider.offsetWidth +'px';
     leftBlock.style.height = wrapper.offsetHeight + 'px';
     imgLeft.style.width = wrapper.offsetWidth + divider.offsetWidth +'px';
@@ -27,25 +25,44 @@ const divider = document.querySelector(".divider"),
 
     divider.style.left = "50%";
     leftBlock.style.width = (wrapper.offsetWidth/2) + 'px';
+
+
+    function moveAt(e) {
     
+      let posX;
+
+        if(e.which=="1"){
+          posX = (e.pageX-wrapper.getBoundingClientRect().x) - divider.offsetWidth / 2;
+        };
+
+        if(e.which=="0"){
+          posX = (e.targetTouches[0].clientX-wrapper.getBoundingClientRect().x) - divider.offsetWidth / 2;
+        };
+
+        if ((posX>=0)&&(posX<=wrapper.offsetWidth)){
+        divider.style.left = posX  + 'px';
+        leftBlock.style.width = posX + 'px';        
+        }
+   
+    }
+
 
   divider.addEventListener('mousedown', (e)=>{
 
-    function moveAt(e) {
-      let posX = e.pageX-divider.offsetWidth / 2;
-  
-      if ((posX>=0)&&(posX<=wrapper.offsetWidth*2)){
-        divider.style.left = posX  + 'px';
-        leftBlock.style.width = posX + 'px';        
-      }      
-    }
-
     wrapper.onmousemove = function(e) {
       moveAt(e);
-      console.log(e.pageX);
-      console.log(divider.getBoundingClientRect());
-     }
-});
+    }
+
+  });
+
+  divider.addEventListener("touchstart", (e)=>{
+        
+    divider.addEventListener('touchmove', function(e){
+      moveAt(e);
+    });
+    
+  });
+
 
   divider.addEventListener("mouseup", ()=>{
     wrapper.onmousemove = null;
@@ -55,3 +72,5 @@ const divider = document.querySelector(".divider"),
   wrapper.onmousedown = function(){
       return false;
   };
+    
+});
